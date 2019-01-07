5644e573b9389ae398b36c297cdb98c1ba4c985e
Anton <apiskareff@gmail.com>
2019-01-08 00:06:46 +0400

    Added UI panels change
    Added UI panels change

--- Assets/Scripts/CameraMoving.cs
+++ Assets/Scripts/CameraMoving.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,10 +7,23 @@ public class CameraMoving : MonoBehaviour {
 
     public GameObject Origin;
     public GameObject Destination;
+    public static bool UIUpdateMarker;
 
     public void OnMouseDown()
     {
-        Origin.transform.position = new Vector3(0.0f, -1.0f);
-        Destination.transform.position = new Vector3(0.0f, 0.0f);
+    	UIUpdateMarker = true;
+
+//        Origin.transform.position = new Vector3(1.0f, 1.0f);
+//        Destination.transform.position = new Vector3(0.0f, 0.0f);
+    }
+
+    private void Update()
+    {
+    	if (UIUpdateMarker == true)
+    	{
+    	Origin.SetActive(false);
+    	Destination.SetActive(true);
+    	UIUpdateMarker = false;
+    	}
     }
 }
